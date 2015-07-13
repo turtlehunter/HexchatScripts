1fe27b42cfed13ed17d9cf65fcf0a92b5a304657
CubeTheThird <truebix@gmail.com>
2015-07-12 21:33:10 -0400

    Update colourName.py
    Fixed a bug caused by empty matches in the regular expressions.

--- colourName.py
+++ colourName.py
@@ -1,5 +1,5 @@
 __module_name__ = 'Name Colourer'
-__module_version__ = '0.9'
+__module_version__ = '1.0'
 __module_description__ = 'Colours names in chat messages using HexChat colouring'
 
 import hexchat
@@ -27,7 +27,7 @@ def colour_names(word, word_eol, event, attrs):
 		name = user.nick
 		if name_search(message, name):
 			colour = get_colour(name)
-			message = re.sub(r'((?!' + name_start + '))' + name + '((?!=' + name_other + '))',r'\1\003' + str(colour) + name + '\003\3', message)
+			message = re.sub(r'(|(?!' + name_start + '))' + name + '(|(?!=' + name_other + '))',r'\1\003' + str(colour) + name + '\003\3', message)
 	word[1] = message
 	halt = True
 	hexchat.emit_print(event, *word)
